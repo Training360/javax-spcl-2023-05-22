ccd7c7f54be5741bac261dc074bb5f609f5e5da2
Viczián István <viczian.istvan@gmail.com>
2023-05-22 17:04:26 +0200

    Okos entitás

--- course-service/src/main/java/courseservice/course/model/Course.java
+++ course-service/src/main/java/courseservice/course/model/Course.java
@@ -1,5 +1,7 @@
 package courseservice.course.model;
 
+import courseservice.course.dto.CreateCourseCommand;
+import courseservice.course.dto.EnrollCommand;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -33,4 +35,33 @@ public class Course {
     @ElementCollection
     List<Long> completedEmployees;
 
+    public static Course announceCourse(CreateCourseCommand command) {
+        var course = new Course();
+        course.setName(command.getName());
+        course.setDescription(command.getDescription());
+        course.setSyllabus(course.getSyllabus());
+        course.setLimit(course.getLimit());
+        return course;
+    }
+
+    public boolean enroll(EnrollCommand command) {
+        if (alreadyEnrolled(command)) {
+            return true;
+        }
+        if (isFull()) {
+            return false;
+        }
+        else {
+            enrolledEmployees.add(command.getEmployeeId());
+            return true;
+        }
+    }
+
+    private boolean alreadyEnrolled(EnrollCommand command) {
+        return enrolledEmployees.contains(command.getEmployeeId());
+    }
+
+    private boolean isFull() {
+        return enrolledEmployees.size() >= limit;
+    }
 }
--- course-service/src/main/java/courseservice/course/service/CourseService.java
+++ course-service/src/main/java/courseservice/course/service/CourseService.java
@@ -3,9 +3,12 @@ package courseservice.course.service;
 import courseservice.course.dto.CourseDetailsView;
 import courseservice.course.dto.CourseView;
 import courseservice.course.dto.CreateCourseCommand;
+import courseservice.course.dto.EnrollCommand;
+import courseservice.course.model.Course;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,15 +22,24 @@ public class CourseService {
     private CourseMapper courseMapper;
 
     public CourseView createCourse(CreateCourseCommand command) {
-        return null;
+        var course =  Course.announceCourse(command);
+        courseRepository.save(course);
+        return courseMapper.toView(course);
     }
 
     public CourseDetailsView findCourseById(long id) {
-        return null;
+        var course = courseRepository.findById(id).orElseThrow();
+        return courseMapper.toDetailsView(course);
     }
 
     public List<CourseView> findAllCourses() {
-        return null;
+        return courseRepository.findAllView();
+    }
+
+    @Transactional
+    public boolean enroll(EnrollCommand command) {
+        var course = courseRepository.findById(command.getCourseId()).orElseThrow();
+        return course.enroll(command);
     }
 
 }
