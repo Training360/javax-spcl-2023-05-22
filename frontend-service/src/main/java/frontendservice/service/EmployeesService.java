583c0140365f97e185ec9fcf87ef1033f1ec980f
Viczián István <viczian.istvan@gmail.com>
2023-05-22 15:32:49 +0200

    Frontend

---
+++ frontend-service/src/main/java/frontendservice/service/EmployeesService.java
@@ -0,0 +1,27 @@
+package frontendservice.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeesService {
+
+    private EmployeeClient employeeClient;
+
+    public List<Employee> listEmployees() {
+    }
+
+    public List<Role> listRoles() {
+    }
+
+    public void createEmployee(CreateEmployeeCommand command) {
+    }
+
+}
